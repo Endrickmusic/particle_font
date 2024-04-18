621ed673010bdc62f8c42440d44943816fff3fc7
Christian <mail@christianhohenbild.de>
2024-04-18 23:55:51 +0200

    add particles

--- src/Model.jsx
+++ src/Model.jsx
@@ -6,14 +6,16 @@ export default function Model(props) {
   const { nodes, materials } = useGLTF('./models/fraktur_01.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh
+      <points
         castShadow
         receiveShadow
         geometry={nodes.Fraktur_Mag_mesh.geometry}
-        material={nodes.Fraktur_Mag_mesh.material}
+        // material={nodes.Fraktur_Mag_mesh.material}
         position={[-3.5, 0, 0]}
         rotation={[Math.PI / 2, 0, 0]}
-      />
+        >
+      <pointsMaterial color="#5786F5" size={0.0015} sizeAttenuation/>
+      </points>
     </group>
   )
 }
